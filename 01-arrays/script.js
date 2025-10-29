477f3b41ab38a50925a9eed4c530f5bd341159fd
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-10-29 17:43:46 +0100

    feat: finito esercizio e si passa al bonus

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -10,27 +10,34 @@ const teachers = [
 
 // 1. Utilizzando un indice sull'array teachers, cambia il valore della
 // variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
-const fourthTeacher = null;
+const fourthTeacher = teachers[3];
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-teachers.splice( 4 , 1 , "patrick")
+teachers.splice(4, 1, "patrick")
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = teachers.pop()
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
- firstTeacher = teachers.shift();
- 
+firstTeacher = teachers.shift();
+
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 teachers.push("Vanessa")
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = teachers.indexOf("lewis");
+const lewisIndex = teachers.indexOf("Lewis");
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0; // bool
+if (isTeachersEmpty) {
+  console.log("è vuoto")
+}
+else {
+  console.log("ci sta qualcosa")
+}
+
 
 
 
