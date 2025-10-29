9d065d2af2c8363a857c3f16962bfcbfc310643c
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-10-29 18:14:30 +0100

    feat: finita una parte del bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,11 +10,11 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = teachers.slice().reverse();
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = teachers.length >=5;
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
@@ -29,4 +29,4 @@ else{
 }
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+  
