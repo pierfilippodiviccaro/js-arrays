6aa2eef51f9fa12be16293547397748e2352a804
pierfilippodiviccaro <diviccarofilippo@gmail.com>
2025-10-29 16:58:08 +0100

    first commit

---
+++ 01-arrays/script.js
@@ -0,0 +1,38 @@
+const teachers = [
+  'Nathan',
+  'Ed',
+  'Fabio',
+  'Phil',
+  'Carlo',
+  'Lewis',
+  'Luca'
+]; // NON MODIFICARE QUESTA VARIABILE
+
+// 1. Utilizzando un indice sull'array teachers, cambia il valore della
+// variabile fourthTeacher qui sotto in modo che sia il quarto insegnante elencato nell'array
+const fourthTeacher = null;
+
+// 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+teachers.splice( 4 , 1 , "patrick")
+// 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
+const lastTeacher = teachers.pop()
+
+// 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
+ firstTeacher = teachers.shift();
+ 
+// 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa")
+// 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+
+// 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
+// e salvalo nella variabile lewisIndex
+const lewisIndex = teachers.indexOf("lewis");
+
+// 8. Verifica se l'array teachers è vuoto
+// e salva il risultato nella variabile isTeachersEmpty
+const isTeachersEmpty = null;
+
+
+
+
+
